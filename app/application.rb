b49eaa8b32f7ba282f15a193e00f054f2a0585be
Tatyana Scotce <tscotce@gmail.com>
2018-07-13 20:37:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
     #   search_term = req.params["q"]
     #   resp.write handle_search(search_term)
     else
-      resp.write "Route Not Found"
+      resp.write "Route not found"
     end
 
     resp.finish
