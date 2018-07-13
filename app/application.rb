8d632198a3d1838e0a357cfea9424d25c7205377
Tatyana Scotce <tscotce@gmail.com>
2018-07-13 20:55:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,21 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item.name}, #{item.price}\n"
-      end
-    elsif req.path.match(/items/)
       item = req.path.split("/items/").last
-      if !@@items.include?(item) 
+      if @@items.include?(item) 
+        @@items.each do |item|
+          resp.write "#{item.name}, #{item.price}\n"
+        end
+      else 
         resp.write "Item not found\n"
         resp.status = 400
-     end
+      end
+    # elsif req.path.match(/items/)
+    #   item = req.path.split("/items/").last
+    #   if !@@items.include?(item) 
+    #     resp.write "Item not found\n"
+    #     resp.status = 400
+    # end
     else
       resp.write "Route not found\n"
       resp.status = 404
