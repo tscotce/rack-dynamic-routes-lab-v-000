2aae380f9b8a08b7a368f7562bbd76d08adc8a2c
Tatyana Scotce <tscotce@gmail.com>
2018-07-13 20:42:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,7 @@ class Application
     elsif req.path.match(/items/)
       !@@items.contains? 
       resp.write "Item not found"
+      resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 404
